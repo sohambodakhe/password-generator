04acdcc9561200083ec5b2df8230c11e6bf56702
sohmbodakhe <sohmyeahboi@gmail.com>
2026-04-21 16:19:44 +0530

    initial commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,98 +1,79 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { StyleSheet, Text, View , } from 'react-native'
+import React, { use, useState } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import * as Yup from 'yup';
 
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
+const PasswordSchema = Yup.object().shape({
+  passwordlength: Yup.number().min(4, 'Password must be at least 4 characters long').max(16, 'Password must be at most 16 characters long')
+  .required('Length is required'),
+});
+export default function index() {
 
-export default function HomeScreen() {
+  const [password , setPassword] = useState('')
+  const [isPassGenerated , setIsPassGenerated] = useState(false)
+
+  const [lowercase , setLowercase] = useState(true)
+  const [uppercase , setUppercase] = useState(false)
+  const [numbers , setNumbers] = useState(false)
+  const [symbols , setSymbols] = useState(false)
+
+  const generatePasswordString = () => (passwordLength: number) => {
+    let characters = '';
+    if (lowercase) characters += 'abcdefghijklmnopqrstuvwxyz';
+    if (uppercase) characters += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    if (numbers) characters += '0123456789';
+    if (symbols) characters += '!@#$%^&*()_+~`|}{[]:;?><,./-=';
+
+    if(uppercase){
+      characters += uppercase
+
+    }
+    if (lowercase){
+      characters += lowercase
+    }
+    if (numbers){
+      characters += numbers
+    }
+    if (symbols){
+      characters += symbols
+    }
+    const createdPassword = createPassword(characters , passwordLength)
+
+    setPassword(createdPassword)
+    setIsPassGenerated(true)
+  }
+
+  const createPassword = (characters: string , passwordLength: number) => {
+    let result = ''
+    for (let i = 0; i < passwordLength; i++){
+      const characterIndex  = Math.random() * characters.length;
+      result += characters.charAt(characterIndex)
+    }
+    return result
+  }
+
+  const resetPasswordState = () => {
+    setPassword('')
+    setIsPassGenerated(false)
+    setLowercase(true)
+    setUppercase(false)
+    setNumbers(false)
+    setSymbols(false)
+
+  }
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
-            />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
-              />
-            </Link.Menu>
-          </Link.Menu>
-        </Link>
-
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+    <SafeAreaView>
+      <View>
+        <Text style = {styles.headingText}>App</Text>
+      </View>
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+  headingText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color : 'white'
+  }})
